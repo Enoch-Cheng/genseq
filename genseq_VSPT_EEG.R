d1ac4b4863b9cf99fc47316e76630d50aa9541cd
Enoch Cheng <enochcheng@cuhk.edu.hk>
2018-12-19 11:18:56 +0800

    first commit

---
+++ genseq_VSPT_EEG.R
@@ -0,0 +1,158 @@
+#Generate new sequence for VSPT EEG
+# v2b written by Enoch 20181218
+#press "source" to run the whole program!
+#the generated sequence is stored in the dataframe called "output"
+
+#custom function to check whether there is n or less consecutive duplicates in vector x
+checkseq = function(x, n){
+  stopifnot(length(x)>=n)
+  stopifnot(n>=1)
+  
+  good = TRUE
+  for (i in 1:(length(x)-n)){
+    temp = TRUE
+    for(j in 1:n){
+      temp = temp & (x[i]==x[i+j])
+    }
+    if (temp)
+      good = FALSE
+  }
+  return(good)
+}
+
+#custom function for generating a shortest sequence
+#x is the vector to be randomized
+genseq = function (x){
+  #create sequence for each condition
+  num = length(x)
+  a=sample(x,num,FALSE) 
+  b=sample(x,num,FALSE)
+  c=sample(x,num,FALSE)
+  d=sample(x,num,FALSE)
+  #convert four sequences to one
+  k = vector("numeric")
+  for (i in 1:num) {
+    k = append(k,a[i])
+    k = append(k,b[i])
+    k = append(k,c[i])
+    k = append(k,d[i])
+  }
+  return(k)
+}
+
+# custom function for counting the consecutive repetitions of a vector 
+checkrep = function (x){
+  c = vector("numeric")
+  c = append(c,0)
+  p=0
+  for(i in 2:length(x)){
+    if(x[i]==x[i-1]){
+      p = p+1
+      c = append(c,p)
+    }
+    else{
+      c = append(c,0)
+      p=0
+    }
+    
+  }
+  return(c)
+}
+
+# function to generate the matrix to be shuffled
+setseq = function(n){
+  a = c(TRUE,TRUE,FALSE,FALSE)
+  b = c(TRUE,FALSE,TRUE,FALSE)
+  c = !xor(a,b)
+  m = matrix(rep(c(a,b,c),n),nrow = 8,ncol=3)
+  return(m)
+}
+
+# -------------------- settings -----------------
+# per: S = True, O = False
+# stim: L = True, R = False
+# resp: L = True, R = False
+
+m = setseq(8)
+n = nrow(m)
+
+minlen = 8
+len=32  #controls how long the total sequence is & must be a multiple of minlen!
+per_consec = 4#max allowed same consecutive answers
+stim_consec=4
+ans_consec=4 
+
+#generate long sequence by fragments---------------
+finished = FALSE
+c = 1
+while(!finished){
+  
+  print(c)
+  c=c+1
+  pre = matrix(NA, nrow = 0, ncol = 3)
+  s = matrix(NA, nrow = 0, ncol = 3)
+  
+  for (i in 1:(len/minlen)){
+    temp = m[sample(n),]
+    gap = rbind(pre,temp)
+    
+    #print(paste("generating fragment",i,"-----------"))
+    count = 1
+    done = FALSE
+    while(done==FALSE){
+      count = count+1
+      #print(paste("fragment:",i,"cycle:",count))
+      temp = m[sample(n),]
+      gap = rbind(pre,temp)
+      done = checkseq(gap[,1],per_consec)&&checkseq(gap[,2],stim_consec)&&checkseq(gap[,3],ans_consec)
+    }
+    s=rbind(s,temp)
+    pre=temp
+  }
+  
+  # recoding
+  output = data.frame(per = ifelse(s[,1],"S","W")
+                      , stim=ifelse(s[,2], "L","R")
+                      , ans=ifelse(s[,3], "L","R"))
+  
+  # checking
+  output$countper = checkrep(output$per)
+  output$countstim = checkrep(output$stim)
+  output$countans = checkrep(output$ans)
+  output$persw = ifelse(output$countper==0,"Sh","Rp")
+  output$anssw = ifelse(output$countans==0,"Sh","Rp")
+  output$persw[1]="X"
+  output$anssw[1]="X"
+  
+  x = table(output$persw)
+  y = table(output$anssw)
+  
+  if (abs(x[1]-x[2])==1 && abs(y[1]-y[2])==1){
+    z = table(paste0(output$per,output$persw),paste0(output$ans,output$anssw))
+    z = z[c("SRp","SSh","WRp","WSh"),c("LRp","LSh","RRp","RSh")]
+    
+    if(length(which(z==0))==0 && length(which(z==3))==0 && length(which(z==5))==0 && length(which(z==6))==0){
+      finished = TRUE
+    }
+    
+  }
+  
+  
+}# end of while loop
+
+output.nice = data.frame(cond = paste0(output$per,output$persw),
+                         stim = output$stim,
+                         ans = output$ans,
+                         ans_rep = paste0(output$ans,output$anssw))
+
+# code the first item in output.nice
+j = which(z==1,arr.ind=TRUE)
+output.nice$cond[1]=rownames(z)[j[1]]
+output.nice$ans_rep[1]=colnames(z)[j[2]]
+
+
+# double checking ----------------
+#table(paste0(output$per,output$persw),paste0(output$ans,output$anssw))
+#table(paste0(output$per,output$stim,output$ans))
+#table(output$per,paste0(output$ans,output$countans))
+#table(output.nice$cond,output.nice$ans_rep)
