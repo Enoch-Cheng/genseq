7e7835220b156f93ab3a18771f237e84b1fffe59
Enoch Cheng <enochcheng@cuhk.edu.hk>
2018-12-26 23:37:37 +0800

    script to write to excel in different sheets

---
+++ saveseq.R
@@ -0,0 +1,18 @@
+# save sequences into excel file
+library(openxlsx)
+library(dplyr)
+library(stringr)
+index = data.frame(filenames=list.files(pattern = "*.RData"))
+index = index %>% mutate(condans = str_extract(filenames,"[A-z]+")) %>% arrange(condans)
+index = index %>% group_by(condans) %>% mutate(count = row_number())
+#table(substr(index$condans,1,3),substr(index$condans,4,6))
+
+wb = createWorkbook("VSPT_EEG_sequences")
+for(i in 1:3){
+  sheetname = paste0(index$condans[i],index$count[i])
+  addWorksheet(wb, sheetname)
+  load(as.character(index$filenames[i]))
+  writeData(wb, sheet = i, output.nice)
+}
+
+saveWorkbook(wb, "seq1.xlsx", overwrite = TRUE)
